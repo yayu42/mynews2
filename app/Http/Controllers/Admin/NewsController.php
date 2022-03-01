b93d8a86c97e5af7467dfc77749ed8ce4bf467ff
maki <yayu.060842@gmail.com>
2022-03-01 02:21:42 +0000

    scssの更新

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -2,29 +2,15 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class NewsController extends Controller
 {
-    //controller
-       public function add()
-    {
-        return view('admin.profile.create');
-    }
+  // 以下を追記
+  public function add()
+  {
+      return view('admin.news.create');
+  }
 
-    public function create()
-    {
-        return redirect('admin/profile/create');
-    }
-
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
-
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-    }
 }
