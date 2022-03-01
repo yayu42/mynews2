2b0991bc628d9a889ac96d01bee34389a77f41ae
maki <yayu.060842@gmail.com>
2022-03-01 05:02:55 +0000

    提出課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,20 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible"
-content="IE=edga">
-    <meta name="viewport" content="width=device-width,
-initial-scale=1">
-
-    <title>保育にっき</title>
-   </head>
-   
- 　<body>
- 　  <h1 style="color:DarkGreen;">○○保育園</h1>
- 　 <p>すべての子どもには無限の可能性があります。</p>
- 　 <p>私たちは、それを引き出し「その子らしさと自ら伸びるチカラ」を育みます。</p>
- 　 
- 　 </body>
- </html>
- 
+ {{-- layouts/profle.blade.phpを読み込む --}}
+       @extends('layouts.profile')
+       
+       {{--profile.blade.phpの@yield('title)に'ニュースの新規作成'を埋め込む --}}
+       @section('title','Myプロフィール')
+       
+       {{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+       @section('content')
+         <div class="container">
+             <div class="row">
+                 <div class="col-md-8 mx-auto">
+                     <h2>Myプロフィール</h2>
+                 </div>
+             </div>
+         </div>
+    @endsection
