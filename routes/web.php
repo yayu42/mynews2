5e885c5f5bfb1a1cee48f6999b9f904c2038fd1a
maki <yayu.060842@gmail.com>
2022-03-02 10:54:16 +0000

    ユーザー認証の実装　課題

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Route::group(['prefix' => 'admin'],function(){
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
