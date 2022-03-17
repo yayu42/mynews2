395e9f66b0f814f334ef5b499cc7e9493fcac6fa
maki <yayu.060842@gmail.com>
2022-03-17 13:35:08 +0000

    laravel 19

--- routes/web.php
+++ routes/web.php
@@ -37,4 +37,7 @@ Route::group(['prefix' => 'admin','middleware' => 'auth'],function(){
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'NewsController@index');
+Route::get('/profile', 'ProfileController@index');
+
 
