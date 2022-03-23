9dab1a4453321559a20cc91cce58eba4c0f48a5d
maki <yayu.060842@gmail.com>
2022-03-23 11:18:04 +0000

    Heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //
        if (\App::environment('production')) {
-           \URL::forceScheme('https');
-     }
-  }
+          \URL::forceScheme('https');
+       }
+    }
 }
